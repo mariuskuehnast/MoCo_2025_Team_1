589f04e9545e5537d192dc2a7da0f4e28e4cf86b
Marius Kühnast <marius.kuehnast@smail.th-koeln.de>
2025-06-17 00:10:27 +0200

    Added a separate NavHost to the NewEntryModal

--- app/src/main/java/com/example/moco2025team1/ui/composables/NewEntryModal.kt
+++ app/src/main/java/com/example/moco2025team1/ui/composables/NewEntryModal.kt
@@ -1,5 +1,9 @@
 package com.example.moco2025team1.ui.composables
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.EaseOut
+import androidx.compose.animation.core.tween
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -8,41 +12,64 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.moco2025team1.model.entities.Prompt
 import com.example.moco2025team1.ui.screens.PromptSelectionScreen
+import kotlinx.serialization.Serializable
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewEntryModal(onDismissRequest: () -> Unit) {
     val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
-    var modalStage by remember { mutableStateOf(NewEntryModalStage.PromptSelection) }
     var prompt by remember { mutableStateOf<Prompt?>(null) }
+    val navController = rememberNavController()
 
     ModalBottomSheet(sheetState = sheetState, onDismissRequest = onDismissRequest) {
-        when (modalStage) {
-            NewEntryModalStage.PromptSelection -> PromptSelectionScreen(onPromptSelection = {
-                modalStage = NewEntryModalStage.EntryComposer
-                prompt = it
-            })
-
-            NewEntryModalStage.EntryComposer -> EntryComposer(
-                prompt = prompt,
-                onBack = {
-                    modalStage =
-                        NewEntryModalStage.PromptSelection
-                },
-                onConfirm = {
-                    modalStage =
-                        NewEntryModalStage.ContactSelection
+        NavHost(navController, startDestination = PromptSelection) {
+            composable<PromptSelection> {
+                PromptSelectionScreen(onPromptSelection = {
+                    navController.navigate(EntryComposer)
+                    prompt = it
                 })
+            }
+            composable<EntryComposer>(enterTransition = {
+                slideIntoContainer(
+                    animationSpec = tween(200, easing = EaseIn),
+                    towards = AnimatedContentTransitionScope.SlideDirection.Start
+                )
+            }, exitTransition = {
+                slideOutOfContainer(
+                    animationSpec = tween(200, easing = EaseOut),
+                    towards = AnimatedContentTransitionScope.SlideDirection.End
+                )
+            }) {
+                EntryComposer(
+                    prompt = prompt,
+                    onBack = {
+                        navController.navigate(PromptSelection)
+                    },
+                    onConfirm = {
+                        navController.navigate(
+                            ContactSelection
+                        )
+                    })
+            }
+            composable<ContactSelection> {
 
-            NewEntryModalStage.ContactSelection -> TODO()
+            }
         }
     }
 }
 
-enum class NewEntryModalStage {
-    PromptSelection,
-    EntryComposer,
-    ContactSelection
-}
+sealed class NewEntryModalStage
+
+@Serializable
+data object PromptSelection : NewEntryModalStage()
+
+@Serializable
+data object EntryComposer : NewEntryModalStage()
+
+@Serializable
+data object ContactSelection : NewEntryModalStage()
