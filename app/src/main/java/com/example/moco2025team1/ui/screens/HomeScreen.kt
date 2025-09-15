b1696d6f571cf1f385ab85a79f8c6ff134dbf6b4
kesilchen <roberthellmann2003@gmail.com>
2025-09-15 19:43:25 +0200

    Update HomeScreen to show message if friend list is empty

--- app/src/main/java/com/example/moco2025team1/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/moco2025team1/ui/screens/HomeScreen.kt
@@ -1,12 +1,18 @@
 package com.example.moco2025team1.ui.screens
 
-import android.app.Application
+import android.app.Application import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.SentimentDissatisfied
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.platform.LocalContext
 import com.example.moco2025team1.ui.composables.ContactCard
 import com.example.moco2025team1.viewmodel.HomeViewModel
 import com.example.moco2025team1.viewmodel.SessionViewModel
@@ -21,11 +27,38 @@ fun HomeScreen(
     val friends by vm.friends.collectAsState()
 
     if (friends.isEmpty()) {
-        Text("You have no friends yet.")
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 24.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Icon(
+                    imageVector = Icons.Outlined.SentimentDissatisfied,
+                    contentDescription = "No friends",
+                    modifier = Modifier.size(96.dp),
+                )
+                Spacer(Modifier.height(12.dp))
+                Text(
+                    "You have no friends yet",
+                    style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.SemiBold)
+                )
+                Spacer(Modifier.height(4.dp))
+                Text(
+                    "Try adding new friends!",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.75f)
+                )
+            }
+        }
         return
     }
 
-    LazyColumn {
+    LazyColumn(
+        contentPadding = PaddingValues(vertical = 8.dp, horizontal = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
         itemsIndexed(friends) { index, user ->
             ContactCard(user.userName) { onContactClick(index) }
         }
