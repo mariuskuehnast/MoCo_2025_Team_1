56c064f342ad17157a98aa17edc6b15fe40141bf
Marius Kühnast <marius.kuehnast@smail.th-koeln.de>
2025-09-20 13:52:56 +0200

    feat: implement show more button on the entry viewer

--- app/src/main/java/com/example/moco2025team1/ui/screens/EntryViewerScreen.kt
+++ app/src/main/java/com/example/moco2025team1/ui/screens/EntryViewerScreen.kt
@@ -2,6 +2,7 @@ package com.example.moco2025team1.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,21 +12,25 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -56,9 +61,24 @@ fun EntryViewerScreen(
     }
 }
 
+fun shortenContent(content: String): String {
+    val words = content.split(Regex("\\s+"))
+    var newContent = words.take(20).joinToString(" ")
+    if (words.size > 20) {
+        newContent += "..."
+    }
+    return newContent
+}
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EntryViewerScreen(entry: Entry) {
+    val bottomSheetState = rememberModalBottomSheetState()
+    var showMore by remember { mutableStateOf(false) }
+
+    val shortenedContent = shortenContent(entry.content)
+
     Box {
         Image(
             painter = rememberAsyncImagePainter(entry.imageUri),
@@ -98,11 +118,23 @@ fun EntryViewerScreen(entry: Entry) {
             Text(
                 entry.prompt ?: "",
                 color = Color.White,
-                style = MaterialTheme.typography.titleMedium,
+                fontSize = 18.sp,
                 fontWeight = FontWeight.SemiBold
             )
-            Text(entry.content, color = Color.White)
-            Text("Show more", color = Color.White, fontWeight = FontWeight.SemiBold)
+            Text(shortenedContent, color = Color.White)
+            if (shortenedContent != entry.content) {
+                Text(
+                    "Show more",
+                    color = Color.White,
+                    fontWeight = FontWeight.SemiBold,
+                    modifier = Modifier.clickable { showMore = true }
+                )
+            }
+        }
+    }
+    if (showMore) {
+        ModalBottomSheet(onDismissRequest = { showMore = false }, sheetState = bottomSheetState) {
+            Text(entry.content, modifier = Modifier.padding(20.dp, 0.dp))
         }
     }
 }
